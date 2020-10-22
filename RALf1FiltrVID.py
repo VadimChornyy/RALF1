2e1e58f4cabb1337f929c1bf70c60f1b2715489b
Vadim Chornyy (Chernov) <vadxcher@gmail.com>
2020-10-23 00:19:04 +0300

    Add files via upload

--- RALf1FiltrVID.py
+++ RALf1FiltrVID.py
@@ -174,6 +174,7 @@ def RALF1Calculation(arr_bx,Nf,NNew,NChan,D,Nhh):
             NumFri_=RandomQ(sz)                 
             NumFri=np.concatenate((NumFri, NumFri))                  
             NumFri_=np.concatenate((NumFri_, NumFri_))  
+            xzz=RandomQ(zz*100)
             while zz>=0: 
                 w=1
                 while w>0:    
@@ -185,8 +186,8 @@ def RALF1Calculation(arr_bx,Nf,NNew,NChan,D,Nhh):
                                 dQ4=np.zeros((NCh,NCh0),float)
                                 mDD4=np.zeros((NCh,NCh0),float)
                                 for ll in range(NCh0):
-                                    dQ4[:,ll]=dQ3[NumFri[ii+0:ii+NCh+zz],NumFri_[i+ll+zz]]*1.
-                                    mDD4[:,ll]=mDD[NumFri[ii+0:ii+NCh+zz],NumFri_[i+ll+zz]]*1.
+                                    dQ4[:,ll]=dQ3[NumFri[ii+0:ii+NCh+xzz[zz]],NumFri_[i+ll+xzz[zz]]]*1.
+                                    mDD4[:,ll]=mDD[NumFri[ii+0:ii+NCh+xzz[zz]],NumFri_[i+ll+xzz[zz]]]*1.
                                 dQ4mn=np.mean(dQ4*(1-(np.abs(mDD4)<0*D*Koe)))
                                 dQ4=dQ4-dQ4mn                        
                                 dQ4_A=np.asarray(XFilter.RALF1FilterX(  dQ4*(1-(dQ4<0))+mDD4,len(dQ4),len(dQ4[0]),1,0)+dQ4mn,np.float16)
